7722dc7d1d1d988a501e1d45d394a4a12771af02
Bruno <brunob712@hotmail.com>
2020-06-14 20:05:18 -0300

    new tests

--- app.js
+++ app.js
@@ -3,38 +3,44 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const mercadopago = require('mercadopago');
 
-const mercadoPagoPreference = (baseUrl, price, title, picture_url) => {
+const mercadoPagoPreference = (req, baseUrl, price, title, picture_url) => {
     return new Promise((resolve, reject) => {
         mercadopago.configure({
             access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
             integrator_id: "dev_24c65fb163bf11ea96500242ac130004"
         });
-
+        console.log("picture_url: ", picture_url);
+        const editedPictureUrl = req.picture_url ? encodeURIComponent(picture_url.replace("./", "https://brunob-712-mp-commerce-nodejs.herokuapp.com/")) : "";
+        const back_params = `img=${encodeURIComponent(picture_url)}&title=${title}&price=${price}&unit=1`;
+        console.log("back_params:", back_params);
+        console.log(`req.query:
+        ${JSON.stringify(req.query)}
+        `)
         let preference = {
-            notification_url: "http://localhost:3000/notifications",
+            // notification_url: `${baseUrl}/notifications`,
             payer: {
                 phone: { area_code: '11', number: 22223333 },
                 address: { zip_code: '1111', street_name: 'False', street_number: 123 },
                 email: 'test_user_63274575@testuser.com',
-                identification: { number: '', type: '' },
+                identification: { number: '471923173', type: '' },
                 name: 'Lalo',
                 surname: 'Landa'
             },
             back_urls: {
-                success: `${baseUrl}/success`,
-                pending: `${baseUrl}/pending`,
-                failure: `${baseUrl}/failure`
+                success: `https://brunob-712-mp-commerce-nodejs.herokuapp.com/success/${back_params}`,
+                pending: `https://brunob-712-mp-commerce-nodejs.herokuapp.com/pending/${back_params}`,
+                failure: `https://brunob-712-mp-commerce-nodejs.herokuapp.com/failure/${back_params}`
             },
-            "auto_return": "approved",
+            // "auto_return": "approved",
             items: [
                 {
                     id: 1234,
                     title: title,
-                    currency_id: "ARS",
-                    unit_price: price,
-                    quantity: 1,
                     description: "Dispositivo móvil de Tienda e-commerce",
-                    picture_url: picture_url.replace("./", "https://brunob-712-mp-commerce-nodejs.herokuapp.com/"),
+                    picture_url: editedPictureUrl,
+                    quantity: 1,
+                    unit_price: price,
+                    currency_id: "ARS",
                     external_reference: "brunob712@hotmail.com",
                 }
             ],
@@ -52,13 +58,18 @@ const mercadoPagoPreference = (baseUrl, price, title, picture_url) => {
                 installments: 6
             }
         };
-        mercadopago.preferences.create(preference)
-            .then((response) => {
-                resolve(response.body.id);
-            }).catch(function (error) {
-                console.log(error);
-                reject(error);
-            });
+        console.log("req.query.price: ", req.query.price)
+        if (req.query.price) {
+            console.log("preference:", preference)
+            mercadopago.preferences
+                .create(preference)
+                .then((response) => {
+                    resolve(response.body.id);
+                }).catch(function (error) {
+                    console.log(error);
+                    reject(error);
+                });
+        }
     })
 }
 
@@ -74,15 +85,17 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     const baseUrl = req.protocol + "://" + req.headers.host;
-    mercadoPagoPreference(baseUrl, parseInt(req.query.price), req.query.title, req.query.img)
-        .then((id) => {
-            req.query.id = id;
-            res.render('detail', req.query);
-        })
+    if (req.query) {
+        mercadoPagoPreference(req, baseUrl, parseInt(req.query.price), req.query.title, req.query.img)
+            .then((id) => {
+                req.query.id = id;
+                res.render('detail', req.query);
+            })
+    }
 });
 
 app.get('/success', (req, res) => {
-    res.render('success', req.query);
+    res.render('success', req.query)
 });
 app.get('/pending', (req, res) => {
     res.render('pending', req.query);
